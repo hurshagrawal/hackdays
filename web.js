4467821ec533784e39c013a6cea3a1b01128dffb
Hursh <hsa470@gmail.com>
2011-07-07 13:44:53 -0400

    Switched to jade

--- web.js
+++ web.js
@@ -1,17 +1,35 @@
+/**
+ * Module dependencies.
+ */
+
 var express = require('express');
 
-var app = express.createServer();
+var app = module.exports = express.createServer();
 
-app.configure(function() {
-	app.use(express.logger());
-	app.use(express.static(__dirname + '/public'));
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser());
+  app.use(express.session({ secret: 'your secret here' }));
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
 });
 
-//--ROUTES--
-app.set('views', __dirname + '/views');
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
 
-app.get('/', function(req, res) {
-	res.render('root.ejs');
+app.configure('production', function(){
+  app.use(express.errorHandler()); 
+});
+
+// Routes
+app.get('/', function(req, res){
+  res.render('index.ejs');
 });
 
 var port = process.env.PORT || 3000;
